52cdf8988ed2c0155262e4dcb26d3788942a3ad3
andrewtiu33 <tiua@oregonstate.edu>
2021-11-10 09:58:46 -0600

    codecomm
    Implemented scraping from bing

---
+++ imagescraper.py
@@ -0,0 +1,61 @@
+from bs4 import BeautifulSoup
+import json
+import requests
+import socket
+
+def runImageScraper():
+    host = socket.gethostname()
+    port = 7439
+
+    # creates the socket 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # binds host and port
+    s.bind((host, port))
+
+    # server listens with a buffer of 6
+    s.listen(6)
+
+    while True:
+        # accepts and establishes a new connection
+        clientsock, address = s.accept()
+        print("connection established with: " + str(address))
+
+        # decode the client request and save the query input
+        request = clientsock.recv(1024).decode("utf-8")
+        jsonrequest = json.loads(request)
+        query = jsonrequest["query"]
+
+        # make the bing search based on the user's inputs
+        bingsearch = "https://bing.com/images/search?q=" + query
+        
+        # use a user agent to search
+        user_agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0'}
+        bingresponse = requests.get(bingsearch, headers=user_agent)
+ 
+        # use beautiful soup to find images related to that query and save them to a dict object
+        soup = BeautifulSoup(bingresponse.content, "html.parser")
+        images = soup.find_all('a', class_='iusc')
+
+        # Return only 15 results
+        image_list = {}
+        i = 1
+        images = images[0:15]
+        for image in images:
+            m = json.loads(image["m"])
+            murl = m["murl"]
+            image_list[str(query) + str(i)] = murl
+            i += 1
+            
+        # convert the dictionary object to a json object and send to client
+        response = json.dumps(image_list)
+        clientsock.send(response.encode("utf-8"))
+        clientsock.close()
+
+if __name__ == '__main__':
+    runImageScraper()
+       
+
+
+
+
---
+++ testclient.py
@@ -0,0 +1,24 @@
+import socket 
+import json
+
+def runClient():
+    host = socket.gethostname()
+    port = 7439
+
+    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    c.connect((host, port))
+
+    message = {"query":"dog"}
+    request = json.dumps(message)
+
+    c.send(request.encode("utf-8"))
+    response = c.recv(8192).decode("utf-8")
+
+    jsonresponse = json.loads(response)
+    print(jsonresponse)
+
+    c.close()
+
+if __name__ == '__main__':
+    runClient()
+
